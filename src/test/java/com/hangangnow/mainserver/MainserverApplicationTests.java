716f6512ef4478c01b58d2f3bed348ae9f329dfd
KIJUNG-CHAE <daiywlf5251@gmail.com>
2022-06-23 21:34:45 +0900

    Feat: init

---
+++ src/test/java/com/hangangnow/mainserver/MainserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangangnow.mainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
