bbc79451b36ff70d6a29b0b65bd4b3702d9f4034
kim0914 <kosa0914@naver.com>
2022-07-10 00:29:04 +0900

    Feat: Add spring security configuration

--- src/main/java/com/hangangnow/mainserver/config/jwt/JwtFilter.java
+++ src/main/java/com/hangangnow/mainserver/config/jwt/JwtFilter.java
@@ -19,6 +19,20 @@ public class JwtFilter extends OncePerRequestFilter {
     public static final String BEARER_PREFIX = "Bearer ";
     private final TokenProvider tokenProvider;
 
+    // 토큰 인증 정보를 현재 Thread SecurityContext 에 저장
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // 1. Request 헤더에서 토큰 꺼냄
+        String jwt = resolveToken(request);
+
+        // 2. 토큰 유효성 검사 -> 정상 토큰이면 Authentication 가져와서 SecurityContext 에 저장
+        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
+            Authentication authentication = tokenProvider.getAuthentication(jwt);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
 
     private String resolveToken(HttpServletRequest request) {
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
@@ -26,17 +40,5 @@ public class JwtFilter extends OncePerRequestFilter {
             return bearerToken.substring(7);
         }
         return null;
-    }
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String jwt = resolveToken(request);
-
-        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-            Authentication authentication = tokenProvider.getAuthentication(jwt);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
-        filterChain.doFilter(request, response);
     }
 }
