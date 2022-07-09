dc71564d71d8d45654036a7d7b17c3ff2fa846f1
kim0914 <kosa0914@naver.com>
2022-07-10 00:28:25 +0900

    Feat: Add refresh token and token repo

---
+++ src/main/java/com/hangangnow/mainserver/repository/RefreshTokenRepository.java
@@ -0,0 +1,39 @@
+package com.hangangnow.mainserver.repository;
+
+import com.hangangnow.mainserver.domain.member.RefreshToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+
+public class RefreshTokenRepository {
+
+    private final EntityManager em;
+
+    public RefreshToken save(RefreshToken refreshToken){
+        em.persist(refreshToken);
+        return refreshToken;
+    }
+
+
+    public void delete(Long key) {
+        em.createQuery("delete from RefreshToken rt where rt.key =:key")
+                .setParameter("key", key)
+                .executeUpdate();
+    }
+
+
+    public Optional<RefreshToken> findByKey(String key){
+        List<RefreshToken> refreshTokens = em.createQuery("select r from RefreshToken r where r.key =:key", RefreshToken.class)
+                .setParameter("key", key)
+                .getResultList();
+
+        return refreshTokens.stream().findAny();
+    }
+}
