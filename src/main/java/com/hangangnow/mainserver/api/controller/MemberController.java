276f3d5204c8974cfc2fb0a5df6f60589d135073
kim0914 <kosa0914@naver.com>
2022-07-13 23:21:08 +0900

    Fix: member password change,

--- src/main/java/com/hangangnow/mainserver/api/controller/MemberController.java
+++ src/main/java/com/hangangnow/mainserver/api/controller/MemberController.java
@@ -1,10 +1,12 @@
 package com.hangangnow.mainserver.api.controller;
 
+import com.hangangnow.mainserver.domain.common.ResponseDto;
 import com.hangangnow.mainserver.domain.member.dto.MemberResponseDto;
 import com.hangangnow.mainserver.domain.member.dto.PasswordRequestDto;
 import com.hangangnow.mainserver.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,33 +19,31 @@ public class MemberController {
 
     @GetMapping("/api/v1/members")
     public ResponseEntity<MemberResponseDto> getMyMemberInfo(){
-        return ResponseEntity.ok(memberService.getLoginMemberInfo());
+        return new ResponseEntity<>(memberService.getLoginMemberInfo(), HttpStatus.OK);
     }
 
 
     @PostMapping("/api/v1/members/logout")
-    public ResponseEntity<String> logout(){
-        return ResponseEntity.ok(memberService.logout());
+    public ResponseEntity<ResponseDto> logout(){
+        return new ResponseEntity<>(memberService.logout(), HttpStatus.OK);
     }
 
 
     @PutMapping("/api/v1/members/password")
-    public ResponseEntity<String> changePassword(PasswordRequestDto passwordRequestDto){
-        return ResponseEntity.ok(memberService.changePassword(passwordRequestDto));
+    public ResponseEntity<ResponseDto> changePassword(@RequestBody PasswordRequestDto passwordRequestDto){
+        return new ResponseEntity<>(memberService.changePassword(passwordRequestDto), HttpStatus.OK);
     }
 
 
-
-
     @DeleteMapping("api/v1/members")
-    public ResponseEntity<String> deleteMember(){
-        return ResponseEntity.ok(memberService.deleteMember());
+    public ResponseEntity<ResponseDto> deleteMember(){
+        return new ResponseEntity<>(memberService.deleteMember(), HttpStatus.OK);
     }
 
 
     @GetMapping("/api/v1/members/{email}")
     public ResponseEntity<MemberResponseDto> getMemberInfo(@PathVariable String email){
-        return ResponseEntity.ok(memberService.getMemberInfoByEmail(email));
+        return new ResponseEntity<>(memberService.getMemberInfoByEmail(email), HttpStatus.OK);
     }
 
 
