fe7abc6556f71d4fcc6c86a542d174166b3bbec0
kim0914 <kosa0914@naver.com>
2022-07-22 21:27:25 +0900

    Feat: Add reg validation and member provider
    DTO 검증 정규식 추가
    로그인 서비스 구분하기 위한 멤버 프로바이더 추가

--- src/main/java/com/hangangnow/mainserver/domain/member/dto/MemberLoginRequestDto.java
+++ src/main/java/com/hangangnow/mainserver/domain/member/dto/MemberLoginRequestDto.java
@@ -3,9 +3,17 @@ package com.hangangnow.mainserver.domain.member.dto;
 import lombok.Data;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 public class MemberLoginRequestDto {
+    @NotBlank(message = "아이디는 필수 값 입니다.")
+    @Pattern(regexp = "^[a-z0-9]{5,20}$", message = "아이디는 5~20자의 영문 소문자, 숫자만 사용 가능합니다.")
     private String loginId;
+
+    @NotBlank(message = "비밀번호는 필수 값 입니다.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=\\S+$).{8,15}", message = "비밀번호는 영문 대,소문자와 숫자가 포함된 8자 ~ 15자의 비밀번호입니다.")
     private String password;
 
     public UsernamePasswordAuthenticationToken toAuthentication() {
