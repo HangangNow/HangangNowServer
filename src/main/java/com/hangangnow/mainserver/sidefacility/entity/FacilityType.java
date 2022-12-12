2861a343561744b2a4f3ba4df74a269a8e473c1c
kim0914 <kosa0914@naver.com>
2022-12-12 18:42:41 +0900

    Refactor: change FacilityType get method to enum function

--- src/main/java/com/hangangnow/mainserver/sidefacility/entity/FacilityType.java
+++ src/main/java/com/hangangnow/mainserver/sidefacility/entity/FacilityType.java
@@ -1,7 +1,43 @@
 package com.hangangnow.mainserver.sidefacility.entity;
 
+import java.util.Arrays;
+
 public enum FacilityType {
-    TOILET, SUN_SHADOW, BICYCLE, STORE, VIEW, DELIVERY_ZONE,
-    LOAD_FOOD, BASKETBALL, BASEBALL, SOCCER, TENNIS, SWIM,
-    CAFE, RESTAURANT, PARKING
+    TOILET("TOILET"),
+    SUN_SHADOW("SUN_SHADOW"),
+    BICYCLE("BICYCLE"),
+    STORE("STORE"),
+    VIEW("VIEW"),
+    DELIVERY_ZONE("DELIVERY_ZONE"),
+    LOAD_FOOD("LOAD_FOOD"),
+    BASKETBALL("BASKETBALL"),
+    BASEBALL("BASEBALL"),
+    SOCCER("SOCCER"),
+    TENNIS("TENNIS"),
+    SWIM("SWIM"),
+    CAFE("CAFE"),
+    RESTAURANT("RESTAURANT"),
+    PARKING("PARKING");
+
+    private final String name;
+
+    FacilityType(String name) {
+        this.name = name;
+    }
+
+    public static FacilityType getFacilityType(String type) {
+        return Arrays.stream(values())
+                .filter(facilityType -> facilityType.name.equals(type))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("주변시설 입력 값을 확인하세요"));
+    }
 }
+
+
+
+
+
+
+
+
+
--- src/main/java/com/hangangnow/mainserver/sidefacility/service/SideFacilityService.java
+++ src/main/java/com/hangangnow/mainserver/sidefacility/service/SideFacilityService.java
@@ -49,30 +49,37 @@ public class SideFacilityService {
     private String key;
 
     @Transactional
-    public GenericResponseDto registerFacility(FacilityRequestDto facilityRequestDto){
+    public GenericResponseDto registerFacility(FacilityRequestDto facilityRequestDto) {
         Park findPark = parkRepository.findByName(facilityRequestDto.getParkName())
                 .orElseThrow(() -> new IllegalArgumentException("해당 한강공원이 존재하지 않습니다"));
 
         Address address = new Address(facilityRequestDto.getAddress());
         Local local = new Local(facilityRequestDto.getName(), facilityRequestDto.getX_pos(), facilityRequestDto.getY_pos());
-        SideFacility sideFacility = new SideFacility();
-        sideFacility.setSideFacility(address, local, findPark, facilityRequestDto.getType());
 
+        SideFacility sideFacility = new SideFacility(address, local, findPark, FacilityType.getFacilityType(facilityRequestDto.getType()));
         sideFacilityRepository.save(sideFacility);
 
-        FacilityResponseDto facilityResponseDto = new FacilityResponseDto();
-        facilityResponseDto.setAddress(sideFacility.getAddress().fullAddress());
-        facilityResponseDto.setName(sideFacility.getLocal().getLocalname());
-        facilityResponseDto.setX_pos(sideFacility.getLocal().getX_pos());
-        facilityResponseDto.setY_pos(sideFacility.getLocal().getY_pos());
+        FacilityResponseDto facilityResponseDto = new FacilityResponseDto(sideFacility);
         return new GenericResponseDto(facilityResponseDto);
+    }
+
+    @Transactional
+    public void save(SideFacility sideFacility) {
+        sideFacilityRepository.save(sideFacility);
+    }
+
+    public FacilityResponseDto getSideFacilityByXposAndYpos(Double x, Double y) {
+        SideFacility sideFacility = sideFacilityRepository.findByFacilityWithXY(x, y)
+                .orElseThrow(() -> new IllegalArgumentException("해당 위도, 경도에 해당하는 주변시설이 없습니다."));
+
+        return new FacilityResponseDto(sideFacility);
     }
 
     public GenericResponseDto getFacilities(String x, String y, String category) {
 
         // 편의점, 주차장, 식당, 카페
-        String[] categories = new String[]{"CS2", "PK6", "FD6", "CE7"};
-        if (!Arrays.asList(categories).contains(category)){
+        List<String> categories = List.of("CS2", "PK6", "FD6", "CE7");
+        if (!categories.contains(category)) {
             throw new IllegalArgumentException("존재하지 않는 카테고리 코드 입니다");
         }
 
@@ -96,9 +103,9 @@ public class SideFacilityService {
 
             int responseCode = con.getResponseCode();
             BufferedReader br;
-            if (responseCode == 200) { // 정상 호출
+            if (responseCode == 200) {
                 br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {  // 에러 발생
+            } else {
                 br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
             }
             String inputLine;
@@ -129,7 +136,7 @@ public class SideFacilityService {
             for (int i = 0; i < jsonArr.size(); i++) {
                 JSONObject jsonObj = (JSONObject) jsonArr.get(i);
 
-                FacilityKakaoResponseDto facilityKakaoResponseDto = new FacilityKakaoResponseDto(String.valueOf(jsonObj.get("place_name")), String.valueOf(jsonObj.get("address_name")) , String.valueOf(jsonObj.get("phone")),
+                FacilityKakaoResponseDto facilityKakaoResponseDto = new FacilityKakaoResponseDto(String.valueOf(jsonObj.get("place_name")), String.valueOf(jsonObj.get("address_name")), String.valueOf(jsonObj.get("phone")),
                         String.valueOf(jsonObj.get("place_url")), Double.valueOf(String.valueOf(jsonObj.get("distance"))), Double.valueOf(String.valueOf(jsonObj.get("x"))), Double.valueOf(String.valueOf(jsonObj.get("y"))));
 
                 facilityKakaoResponseDtos.add(facilityKakaoResponseDto);
@@ -140,15 +147,15 @@ public class SideFacilityService {
     }
 
 
-    public GenericResponseDto getFacilitiesByParkIdAndType(Long parkId, String type){
-        String[] categories = new String[]{"TOILET", "SUN_SHADOW", "BICYCLE", "STORE", "VIEW", "DELIVERY_ZONE", "STORE",
-                "CAFE", "RESTAURANT", "PARKING","LOAD_FOOD", "BASKETBALL", "BASEBALL", "SOCCER", "TENNIS", "SWIM"};
+    public GenericResponseDto getFacilitiesByParkIdAndType(Long parkId, String type) {
+        List<String> categories = List.of("TOILET", "SUN_SHADOW", "BICYCLE", "STORE", "VIEW", "DELIVERY_ZONE", "STORE",
+                "CAFE", "RESTAURANT", "PARKING", "LOAD_FOOD", "BASKETBALL", "BASEBALL", "SOCCER", "TENNIS", "SWIM");
 
-        if (!Arrays.asList(categories).contains(type)){
+        if (!categories.contains(type)) {
             throw new IllegalArgumentException("존재하지 않는 카테고리 코드 입니다");
         }
 
-        FacilityType facilityType = getType(type);
+        FacilityType facilityType = FacilityType.getFacilityType(type);
 
         parkRepository.findById(parkId)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID를 가진 공원이 존재하지 않습니다."));
@@ -160,56 +167,5 @@ public class SideFacilityService {
 
         return new GenericResponseDto(results);
 
-    }
-
-    private FacilityType getType(String type) {
-        // TOILET, SUN_SHADOW, BICYCLE, STORE, VIEW, DELIVERY_ZONE,
-        // LOAD_FOOD, BASKETBALL, SOCCER, TENNIS, SWIM, STORE, CAFE, RESTAURANT, PARKING
-        switch (type) {
-            case "TOILET":
-                return FacilityType.TOILET;
-            case "SUN_SHADOW":
-                return FacilityType.SUN_SHADOW;
-            case "BICYCLE":
-                return FacilityType.BICYCLE;
-            case "STORE":
-                return FacilityType.STORE;
-            case "CAFE":
-                return FacilityType.CAFE;
-            case "RESTAURANT":
-                return FacilityType.RESTAURANT;
-            case "PARKING":
-                return FacilityType.PARKING;
-            case "VIEW":
-                return FacilityType.VIEW;
-            case "DELIVERY_ZONE":
-                return FacilityType.DELIVERY_ZONE;
-            case "LOAD_FOOD":
-                return FacilityType.LOAD_FOOD;
-            case "BASKETBALL":
-                return FacilityType.BASKETBALL;
-            case "SOCCER":
-                return FacilityType.SOCCER;
-            case "TENNIS":
-                return FacilityType.TENNIS;
-            case "SWIM":
-                return FacilityType.SWIM;
-            case "BASEBALL":
-                return FacilityType.BASEBALL;
-            default:
-                return null;
-        }
-    }
-
-    @Transactional
-    public void save(SideFacility sideFacility){
-        sideFacilityRepository.save(sideFacility);
-    }
-
-    public FacilityResponseDto getSideFacilityByXposAndYpos(Double x, Double y){
-        SideFacility sideFacility = sideFacilityRepository.findByFacilityWithXY(x, y)
-                .orElseThrow(() -> new IllegalArgumentException("해당 위도, 경도에 해당하는 주변시설이 없습니다."));
-
-        return new FacilityResponseDto(sideFacility);
     }
 }
